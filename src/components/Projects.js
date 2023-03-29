fedceba83e9f99ba20ee3a596d3dae33094fbb88
Kris Hvattum <klhvattum@live.com>
2023-03-28 21:03:56 -0700

    Update Kardia link to deployed site, resize title, style project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,21 +5,25 @@ const Projects = () => {
     return (
         <div className='sub-section-alternative' id='projects' >
             <h2 className='project-title'>Projects</h2>
+
             <div className='project-container'>
+                <a className='project-link' href='https://brittanyherzog.com' target='_blank' >
                 <div className='project-card'>
                     <img className='project-image' src={BHPhoto} alt='BH Photography project screenshot' />
                     <h3 className='project-card-title'>Photography Website</h3>
                     <p className='subtext'>Photography portfolio website built using ReactJS.</p>
-                    <hr />
-                    <p className='subtext'><a className='project-link' href='https://brittanyherzog.com' target='_blank' >View Here</a></p>
+
                 </div>
-                <div className='project-card'>
-                    <img className='project-image' src={KardiaPhoto} alt='Kardia Classical School project screenshot' />
-                    <h3 className='project-card-title'>Private School Website</h3>
-                    <p className='subtext'>A website built using ReactJS for Kardia Classical Scool (in progress)</p>
-                    <hr />
-                    <p className='subtext'><a className='project-link' href='https://github.com/kristofer11/kardia' target='_blank' >View Here</a></p>
-                </div>
+</a>
+                      <a className='project-link' href='https://kardiaclassical.org' target='_blank' >    
+                    <div className='project-card'>
+              
+                        <img className='project-image' src={KardiaPhoto} alt='Kardia Classical School project screenshot' />
+                        
+                        <h3 className='project-card-title'>Private School Website</h3>
+                        <p className='subtext'>Website built using ReactJS for Kardia Classical Scool</p>
+                    </div>
+                </a>
                 {/* <div className='project-card'>
                     <img className='project-image' src={BHPhoto} alt='BH Photography project screenshot' />
                     <h3 className='project-card-title'>Photography Website</h3>
