21d05d290b204093d02b2206924b11e0c70ad921
Kris Hvattum <klhvattum@live.com>
2023-04-25 21:34:43 -0700

    New project added (country guesser)

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import BHPhoto from '../app/images/Screenshot 2023-02-28 at 7.10.53 PM.jpeg';
 import KardiaPhoto from '../app/images/kardia-3-18.png';
+import CountryGuesser from '../app/images/country-guesser.png'
 
 const Projects = () => {
     return (
@@ -8,20 +9,29 @@ const Projects = () => {
 
             <div className='project-container'>
                 <a className='project-link' href='https://brittanyherzog.com' target='_blank' >
-                <div className='project-card'>
-                    <img className='project-image' src={BHPhoto} alt='BH Photography project screenshot' />
-                    <h3 className='project-card-title'>Photography Website</h3>
-                    <p className='subtext'>Photography portfolio website built using ReactJS.</p>
-
-                </div>
-</a>
-                      <a className='project-link' href='https://kardiaclassical.org' target='_blank' >    
                     <div className='project-card'>
-              
+                        <img className='project-image' src={BHPhoto} alt='BH Photography project screenshot' />
+                        <h3 className='project-card-title'>Photography Website</h3>
+                        <p className='subtext'>Photography portfolio website built using ReactJS.</p>
+
+                    </div>
+                </a>
+                <a className='project-link' href='https://kardiaclassical.org' target='_blank' >
+                    <div className='project-card'>
+
                         <img className='project-image' src={KardiaPhoto} alt='Kardia Classical School project screenshot' />
-                        
+
                         <h3 className='project-card-title'>Private School Website</h3>
                         <p className='subtext'>Website built using ReactJS for Kardia Classical Scool</p>
+                    </div>
+                </a>
+                <a className='project-link' href='https://kardiaclassical.org' target='_blank' >
+                    <div className='project-card'>
+
+                        <img className='project-image' src={CountryGuesser} alt='Screenshot of country guessing game website' />
+
+                        <h3 className='project-card-title'>Country Guessing Game</h3>
+                        <p className='subtext'>Simple educational game that uses the REST Countries API</p>
                     </div>
                 </a>
                 {/* <div className='project-card'>
