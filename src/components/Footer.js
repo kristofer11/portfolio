54f6315d063d1f5918085bc5c91a02b37255e0cd
Kris Hvattum <klhvattum@live.com>
2023-06-12 20:19:34 -0700

    Fix email address in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className='footer' id='contact' >
             <h2>Contact me</h2>
 
-            <a className='email-link' href="mailto:john@example.com">
+            <a className='email-link' href="mailto:klhvattum@live.com">
                 <div className="contact-item">
                     <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="#52d6f4" viewBox="0 0 24 24"><path d="M0 3v18h24v-18h-24zm22 16l-6.526-6.618-3.445 3.483-3.418-3.525-6.611 6.66 5.051-8-5.051-6 10.029 7.446 9.971-7.446-4.998 6.01 4.998 7.99z" /></svg>
                     <p >klhvattum@live.com</p>
