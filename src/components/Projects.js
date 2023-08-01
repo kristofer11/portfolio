89519cf360889aaad3aa4493e67fb60dc8773b4a
Kris Hvattum <klhvattum@live.com>
2023-08-01 13:46:44 -0700

    adjust project layout

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,45 +12,36 @@ const Projects = () => {
             <div className='project-container'>
                 <a className='project-link' href='https://brittanyherzog.com' target='_blank' >
                     <div className='project-card'>
-                        <img className='project-image' src={BHPhoto} alt='BH Photography project screenshot' />
                         <h3 className='project-card-title'>Photography Website</h3>
+                        <img className='project-image' src={BHPhoto} alt='BH Photography project screenshot' />
                         <p className='subtext'>Photography portfolio website built using ReactJS.</p>
-
                     </div>
                 </a>
                 <a className='project-link' href='https://kardiaclassical.org' target='_blank' >
                     <div className='project-card'>
-
-                        <img className='project-image' src={KardiaPhoto} alt='Kardia Classical School project screenshot' />
-
                         <h3 className='project-card-title'>Private School Website</h3>
+                        <img className='project-image' src={KardiaPhoto} alt='Kardia Classical School project screenshot' />
                         <p className='subtext'>Website built using ReactJS for Kardia Classical Scool</p>
                     </div>
                 </a>
                 <a className='project-link' href='https://library-manager-gamma.vercel.app/' target='_blank' >
                     <div className='project-card'>
-
-                        <img className='project-image' src={MyLibrary} alt='Kardia Classical School project screenshot' />
-
                         <h3 className='project-card-title'>Personal Library Manager</h3>
+                        <img className='project-image' src={MyLibrary} alt='Kardia Classical School project screenshot' />
                         <p className='subtext'>*IN PROGRESS* user can to track, rate and (eventually) share what they are reading</p>
                     </div>
                 </a>
                 <a className='project-link' href='https://kristofer11.github.io/country-guesser' target='_blank' >
                     <div className='project-card'>
-
-                        <img className='project-image' src={CountryGuesser} alt='Screenshot of country guessing game website' />
-
                         <h3 className='project-card-title'>Country Guessing Game</h3>
+                        <img className='project-image' src={CountryGuesser} alt='Screenshot of country guessing game website' />
                         <p className='subtext'>Simple educational game that uses the REST Countries API</p>
                     </div>
                 </a>
                 <a className='project-link' href='https://kristofer11.github.io/weather-app' target='_blank' >
                     <div className='project-card'>
-
-                        <img className='project-image' src={WeatherApp} alt='Screenshot of country guessing game website' />
-
                         <h3 className='project-card-title'>Weather forecast app.</h3>
+                        <img className='project-image' src={WeatherApp} alt='Screenshot of country guessing game website' />
                         <p className='subtext'>Started as a code-along, now I am progressively adding features like changing background.</p>
                     </div>
                 </a>
